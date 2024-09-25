5621620dbcccc2c115f80ba6715c120f7f506705
saudahmedkadapa <kadapasaud@gmail.com>
2024-09-25 20:07:34 +0530

    Frontend

---
+++ Go-Park/Goparkfrontend/my-app/src/HelloWorld/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>
+            Hello World
+        </h1>
+      
+    </div>
+  )
+}
+
+export default HelloWorld
